70cfb002b7e0d8624c5317df892b8e6a089bb230
khristiansnyder <41035049+khristiansnyder@users.noreply.github.com>
2023-09-06 11:26:44 -0700

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -26,42 +26,21 @@ const Layout = ({ favorites, children }) => {
       <section className="section">
         <div className="container is-fluid">
           <div className="buttons">
-            <a
-              href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a77088cf"
-              className="button is-primary"
-            >
-              State Park
-            </a>
-            <a
-              href="https://kssurfcams.netlify.app/spot/5cc8b55739a02e0001ac69f8"
-              className="button is-primary"
-            >
-              Pier
-            </a>
-            <a
-              href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a770888a"
-              className="button is-primary"
-            >
-              Lowers
-            </a>
-            <a
-              href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a7708887"
-              className="button is-primary"
-            >
-              Uppers
-            </a>
-            <a
-              href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a770888b"
-              className="button is-primary"
-            >
-              Churches
-            </a>
-            <a
-              href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a77088d7"
-              className="button is-primary"
-            >
-              Doheney
-            </a>
+            <a href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a7708890" className="button is-primary">Pipe</a> 
+            <a href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a7708dbf" className="button is-primary">Tahiti</a> 
+            <a href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a77088ed" className="button is-primary">HB</a> 
+            <a href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a7708e54" className="button is-primary">Newport</a> 
+            <a href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a77088f3" className="button is-primary">CDM</a> 
+            <a href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a7708f21" className="button is-primary">Crystal Cove</a> 
+            <a href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a770882e" className="button is-primary">Creek</a>
+            <a href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a77088d7" className="button is-primary">Doho</a> 
+            <a href="https://kssurfcams.netlify.app/spot/5cc8b55739a02e0001ac69f8" className="button is-primary">SC Pier</a> 
+            <a href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a7708830" className="button is-primary">T-Street</a> 
+            <a href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a77088cf" className="button is-primary">Park</a> 
+            <a href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a7708887" className="button is-primary">Uppers</a> 
+            <a href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a770888a" className="button is-primary">Lowers</a> 
+            <a href="https://kssurfcams.netlify.app/spot/5842041f4e65fad6a770888b" className="button is-primary">Church</a> 
+            <a href="https://kssurfcams.netlify.app/spot/584204204e65fad6a77099d4" className="button is-primary">Sano</a>
           </div>
         </div>
       </section>
